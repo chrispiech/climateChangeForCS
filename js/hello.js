4a77ee550c3b65e53da053648acaf69a4835be05
chrispiech <piech@cs.stanford.edu>
2019-04-22 13:59:22 -0700

    a

---
+++ js/hello.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const e = React.createElement;
+
+class ClimateHandout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    return 'hello world'
+  }
+}
+
+const domContainer = document.querySelector('#climateHandout');
+ReactDOM.render(e(ClimateHandout), domContainer);
