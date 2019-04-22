2e808e5b2ba3c477c260ea47d85c7266d944ea9f
chrispiech <piech@cs.stanford.edu>
2019-04-22 14:32:07 -0700

    a

--- js/hello.js
+++ js/hello.js
@@ -1,5 +1,19 @@
-import * from './react.js';
-import * from './reactDom.js'
+
+
+
+var script1 = document.createElement('script');
+script1.src = 'https://unpkg.com/react@16/umd/react.development.js';
+script1.type = 'text/javascript';
+script1.crossOrigin = 'anonymous';
+document.getElementsByTagName('head')[0].appendChild(script1);
+
+var script2 = document.createElement('script');
+script2.src = 'https://unpkg.com/react-dom@16/umd/react-dom.development.js';
+script2.type = 'text/javascript';
+script2.crossOrigin = 'anonymous';
+document.getElementsByTagName('head')[0].appendChild(script2);
+
+
 
 
 const e = React.createElement;
